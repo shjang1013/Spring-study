7e08723e13b87aded74649c5c3390ade9046363e
shjang1013 <shjang113@gmail.com>
2020-09-26 23:58:46 +0900

    Add BoardController & Test

--- src/main/java/com/sohyeon/controller/BoardController.java
+++ src/main/java/com/sohyeon/controller/BoardController.java
@@ -1,17 +1,20 @@
 package com.sohyeon.controller;
 
+import com.sohyeon.domain.BoardVO;
 import com.sohyeon.service.BoardService;
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @Log4j
 @RequestMapping("/board/*")
-@AllArgsConstructor
+@AllArgsConstructor // 이용해서 생성자를 만들고 자동으로 주입하도록 한다.
 public class BoardController {
 
     private BoardService service;
@@ -22,5 +25,14 @@ public class BoardController {
         model.addAttribute("list", service.getList());
     }
 
+    @PostMapping("/register")
+    public String register(BoardVO board, RedirectAttributes rttr) {
+        log.info("register: " + board);   // 등록 작업이 끝난 후 다시 목록 화면으로 이동하기 위함인데, 추가적으로 새롭게 등록된 게시물의 번호를 같이 전달하기 위해 RedirectAttributes를 이용한다.
 
+        service.register(board);
+
+        rttr.addFlashAttribute("result", board.getBno());
+
+        return "redirect:/board/list";
+    }
 }
---
+++ src/test/java/com/sohyeon/controller/BoardControllerTests.java
@@ -0,0 +1,52 @@
+package com.sohyeon.controller;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration // Test for Controller
+@ContextConfiguration(classes = {com.sohyeon.config.RootConfig.class, com.sohyeon.config.ServletConfig.class})
+@Log4j
+public class BoardControllerTests {
+
+    @Setter(onMethod_ = {@Autowired})
+    private WebApplicationContext context;
+
+    private MockMvc mockMvc; // 가짜로 URL과 파라미터 등을 브라우저에서 사용하는 것처럼 만들어서 Controller를 실행해 볼 수 있다.
+
+    @Before
+    public void setup() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+    }
+
+    @Test
+    public void testList() throws Exception {
+        log.info(mockMvc.perform(MockMvcRequestBuilders.get("/board/list"))
+        .andReturn()
+        .getModelAndView()
+        .getModelMap());
+    }
+
+    @Test
+    public void testRegister() throws Exception {
+        String resultPage = mockMvc.perform(MockMvcRequestBuilders.post("/board/register")
+                .param("title", "테스트 새글 제목")
+                .param("content", "테스트 새글 내용")
+                .param("writer", "user00")
+        ).andReturn().getModelAndView().getViewName();
+
+        log.info(resultPage);
+    }
+}
