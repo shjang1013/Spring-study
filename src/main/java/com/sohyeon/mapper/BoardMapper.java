7f5a002c3e6ab85f9f6fd051fa53a84cd32dd101
shjang1013 <shjang113@gmail.com>
2020-10-31 23:56:59 +0900

    Add Paging Project

--- src/main/java/com/sohyeon/mapper/BoardMapper.java
+++ src/main/java/com/sohyeon/mapper/BoardMapper.java
@@ -1,6 +1,7 @@
 package com.sohyeon.mapper;
 
 import com.sohyeon.domain.BoardVO;
+import com.sohyeon.domain.Criteria;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
@@ -9,6 +10,8 @@ public interface BoardMapper {
 
     // @Select("select * from table_board where bno > 0")
     public List<BoardVO> getList();
+
+    public List<BoardVO> getListWithPaging(Criteria cri);
 
     public void insert(BoardVO board);  // useGeneratedKeys, KeyProperty 옵션을 이용해 결과를 리턴받을 수 있음
                                         // mybatis 구문으로 insert를 시도하게 되면, 파라미터로 넘긴 boardVO 객체의 bno값에 insert 했을 때의 key값(bno)이 들어오게 됨
